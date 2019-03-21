559309210289a94f37a3e6bb96fe3c259a85288b
Kohei Asai <me@axross.io>
2019-03-21 10:39:55 -0700

    Modify App.java

--- src/main/java/dev/axross/ciccc/wmad202/treesandtents/App.java
+++ src/main/java/dev/axross/ciccc/wmad202/treesandtents/App.java
@@ -1,6 +1,5 @@
 package dev.axross.ciccc.wmad202.treesandtents;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,7 +8,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");

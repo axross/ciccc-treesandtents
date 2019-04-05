9328493f40b1e8737129f11b41ec8924ac717d74
danismgomes <danismgomes@gmail.com>
2019-04-05 13:09:36 -0700

    Add logic that evaluate if numbers of dogs match with the number on the label

--- src/treesandtents/Model/BoardModel.java
+++ src/treesandtents/Model/BoardModel.java
@@ -3,11 +3,45 @@ package treesandtents.Model;
 import java.awt.*;
 
 public class BoardModel {
-//    private Button[][] boardGrid;
-//
-//    public BoardModel(Button[][] boardGrid) {
-//        this.boardGrid = boardGrid;
-//    }
-//
-//    if
+    private String[][] boardModelGrid;
+
+    public BoardModel(String[][] boardModelGrid) {
+        this.boardModelGrid = boardModelGrid;
+    }
+
+    public String[][] getBoardModelGrid() {
+        return boardModelGrid;
+    }
+    public String getBoardModelGrid(int row, int colunm) {
+        return boardModelGrid[row][colunm];
+    }
+
+    public void setBoardModelGrid(String[][] boardModelGrid) {
+        this.boardModelGrid = boardModelGrid;
+    }
+
+    public void setBoardModelGrid(int row, int colunm, String value) {
+        this.boardModelGrid[row][colunm] = value;
+    }
+
+
+    public void countNumberOfDogs(){
+        int countDogRow = 0;
+        int countDogColunm = 0;
+        for (int i = 1; i < boardModelGrid.length; i++) {
+            for (int j = 1; j < boardModelGrid.length; j++) {
+                if(boardModelGrid[i][j]=="3"){
+                    countDogRow ++;
+                }
+                if (boardModelGrid[j][i] == "3"){
+                    countDogColunm++;
+                }
+
+            }
+            boardModelGrid[i][0] = Integer.toString(countDogRow);
+            boardModelGrid[0][i] = Integer.toString(countDogColunm);
+            countDogRow = 0;
+            countDogColunm = 0;
+        }
+    }
 }
